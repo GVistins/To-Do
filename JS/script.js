2f6564e92c10a96a5007cc624eaf23b05c3cc1a5
Gustavs <vistinsg2@gmail.com>
2021-04-20 15:32:48 +0300

    Added some js, also it doesn't work

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,21 @@
-document.getElementById("taskButton").addEventListener("click", addTask)
+document.getElementById("taskButton").addEventListener("click", addTask);
+tasks = []
 
 function addTask() {
-    console.log("Task added!")
+    let task = {taskInput: taskInput.value};
+    console.log(task);
+    tasks.push(task);
+    taskReveal();
+}
+
+function taskReveal() {
+    let taskList = document.getElementById("taskList");
+    for(let a = 0; a < tasks.length; a++){
+            let task = `
+        <div class = "task">
+                <h70>${taskList[a].task}</h70>
+            </div>
+        `;
+    }
+    taskList.innerHTML = task;
 }
